8637cb396b85e3e63998836f66affb577a826482
admin <1363193860@qq.com>
2019-01-08 20:52:28 +0800

    第二次提交

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -8,5 +8,9 @@ public class Main {
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");
     }
 }
